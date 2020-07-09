37f937760859e9195c282c9fc7811f254f2cf10e
Zhou Wen Bo z00427cx <wen-bo.zhou@siemens.com>
2020-07-09 16:30:25 +0800

    feat: init micro project for qiankun

---
+++ micro-app-angular/src/public-path.js
@@ -0,0 +1,6 @@
+if (window.__POWERED_BY_QIANKUN__) {
+  // 动态设置 webpack publicPath，防止资源加载出错
+  // eslint-disable-next-line no-undef
+  console.log('angular public path');
+  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+}
---
+++ micro-app-vue/src/public-path.js
@@ -0,0 +1,6 @@
+if (window.__POWERED_BY_QIANKUN__) {
+  // 动态设置 webpack publicPath，防止资源加载出错
+  // eslint-disable-next-line no-undef
+  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+  console.log('vue public path');
+}
